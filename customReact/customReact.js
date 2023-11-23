e9e9c23fc0f9ea1ea2d677f6f8f67f01b9091ba2
Bijay B.K <bijaybk307@gmail.com>
2023-11-23 20:45:46 +0545

    Files added

--- customReact/customReact.js
+++ customReact/customReact.js
@@ -1,8 +1,16 @@
 function customRender(reactElement, container){
+    // const domElement = document.createElement(reactElement.type)
+    // domElement.innerHTML = reactElement.children
+    // domElement.setAttribute('href', reactElement.props.href)
+    // domElement.setAttribute('target', reactElement.props.target)
+    // container.appendChild(domElement)
+
     const domElement = document.createElement(reactElement.type)
     domElement.innerHTML = reactElement.children
-    domElement.setAttribute('href', reactElement.props.href)
-    domElement.setAttribute('target', reactElement.props.target)
+    for (const prop in reactElement.props){
+        if(prop === 'children') continue;
+        domElement.setAttribute(prop, reactElement.props [prop])
+    }
     container.appendChild(domElement)
 }
 
--- vite+react/src/main.jsx
+++ vite+react/src/main.jsx
@@ -2,8 +2,28 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp(){
+  return(
+    <div>
+    <h1>Hi! What's up? </h1>
+    </div>
+  )
+}
+
+
+const anotherElement = (
+  <a href="https://google.com" target='_blank'>Visit Google</a>
+)
+
+const reactElement = React.createElement(
+  'a',
+  {href: 'https://google.com', target: '_blank'},
+  'Click me --> GOOGLE'
+)
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  
+    reactElement
+  
 )
