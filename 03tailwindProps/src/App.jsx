ddaa9517e14e0ada920e86f55cc9676184278872
Bijay B.K <bijaybk307@gmail.com>
2023-12-16 17:49:57 +0545

    objects added

--- 03tailwindProps/src/App.jsx
+++ 03tailwindProps/src/App.jsx
@@ -4,12 +4,17 @@ import Card from './components/card'
 
 function App() {
   const [count, setCount] = useState(0)
+  let myObj = {
+    username : "BIjay",
+    age : 21
+  }
+  let myArray = [1,2,3,4]
 
   return (
     <>
       <h1 className='bg-green-400 test-black p-4 rounded-xl mb-4'>Hello Devs!!</h1>  
 
-      <Card />
+      <Card channel="Code With Bijay" someObj = {myObj} someArray = {myArray}/>
       <Card />
 
       {/* <div class="flex font-sans">
--- 03tailwindProps/src/components/card.jsx
+++ 03tailwindProps/src/components/card.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 function card(props) {
+  console.log("props", props);
   return (
     <div className="relative h-[400px] w-[300px] rounded-md">
   <img
